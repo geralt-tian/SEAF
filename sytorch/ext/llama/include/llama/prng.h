3b329fe53255e399db5e74334c3346fc9b9f3855
Renwen Ma <115010200@link.cuhk.edu.cn>
2025-01-18 00:56:17 -0500

    merge bolt and single activation test

--- sytorch/ext/llama/include/llama/prng.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <cryptoTools/Crypto/PRNG.h>
-
-namespace LlamaConfig {
-    extern osuCrypto::PRNG prngs[256];
-}
-
-extern osuCrypto::PRNG prngShared;
--- sytorch/include/sytorch/random.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cryptoTools/Crypto/PRNG.h>
-
-extern osuCrypto::PRNG prngWeights;
-extern osuCrypto::PRNG prngStr;
-
-// samples a random float in range [0, 1)
-double rand_float();
